47e809df7ba297a810af17a7c97b39e8d1559e89
nabilchabab <nabil.chababnabil@gmail.com>
2023-12-28 15:14:29 +0100

    First Commit

---
+++ app/database/Database.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\database;
+require '../../vendor/autoload.php';
+
+
+class Database {
+    private static $cnx;
+
+    public static function connect() {
+        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+  
+
+        self::$cnx = mysqli_connect($_ENV['DB_SERVERNAME'], $_ENV['DB_USERNAME'],  $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
+
+        if (!self::$cnx) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        return self::$cnx;
+    }
+}
+?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./public/dist/**/*.{html,js}"],
   theme: {
     extend: {},
   },
