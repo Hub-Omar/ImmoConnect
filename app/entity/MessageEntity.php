3b3324006598e3ece886f2c3792c7be3a09b04de
Sanalouaf <sanalouafi2003@gmail.com>
2024-01-03 09:21:04 +0100

    ipdate script file and add message entity and dao and model

---
+++ app/entity/MessageEntity.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\models;
+
+require '../../vendor/autoload.php';
+
+class MessageEntity
+{
+    private $id;
+    private $emetteur_id;
+    private $recepteur_id;
+    private $message;
+    private $sent_at;
+
+    public function __construct($id, $emetteur_id, $recepteur_id, $message, $sent_at)
+    {
+        $this->id = $id;
+        $this->emetteur_id = $emetteur_id;
+        $this->recepteur_id = $recepteur_id;
+        $this->message = $message;
+        $this->sent_at = $sent_at;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getEmetteurId()
+    {
+        return $this->emetteur_id;
+    }
+
+    public function setEmetteurId($emetteur_id)
+    {
+        $this->emetteur_id = $emetteur_id;
+    }
+
+    public function getRecepteurId()
+    {
+        return $this->recepteur_id;
+    }
+
+    public function setRecepteurId($recepteur_id)
+    {
+        $this->recepteur_id = $recepteur_id;
+    }
+
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    public function getSentAt()
+    {
+        return $this->sent_at;
+    }
+
+    public function setSentAt($sent_at)
+    {
+        $this->sent_at = $sent_at;
+    }
+}
