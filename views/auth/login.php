9dd968631ce35f752bba7d6e0341d176bc62d38a
Sanalouaf <sanalouafi2003@gmail.com>
2024-01-03 15:47:48 +0100

    merge with aicha2

--- views/auth/login.php
+++ views/auth/login.php
@@ -20,32 +20,30 @@
                 <h1 class="text-3xl text-white font-bold text-center">
                     Log In
                 </h1>
-                <p class="text-white font-sans text-center">Sign in with  your account !</p>
+                <p class="text-white font-sans text-center">Sign in with your account !</p>
                 <div class="form-input mb-2 pt-5">
-                <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" name = "email" required>
+                    <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" name="email" required>
                     <span id="emailError" class="error-message"></span>
                 </div>
-                
+
                 <div class="form-input mb-2 pt-2">
-                    <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="password" name = "password" required>
+                    <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="password" name="password" required>
                     <span id="passwordError" class="error-message"></span>
                 </div>
-               
+
 
                 <div class="form-input mb-5 pt-2">
                     <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-<<<<<<< HEAD
                     <a href="signup" class=" text-black p-5">Create an account !!<span class="text-white hover:text-blue-500 p-1">Sign Up</span></a>
 
-                 </div>
-=======
                 </div>
->>>>>>> 683420a395402bd9aaed96c2ad137d87e48010b7
-            </form>
+        </div>
+        </form>
         </div>
     </section>
-    
+
 </body>
 
 <script src="/ImmoConnect/public//js/sign_login.js"></script>
+
 </html>
