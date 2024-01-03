e38c5ea18e7cd833dc8eae65944ba6e54017dac2
Sanalouaf <sanalouafi2003@gmail.com>
2024-01-03 23:26:41 +0100

    add message route

--- app/routes/App.php
+++ app/routes/App.php
@@ -11,6 +11,7 @@ $router->setRoutes([
         'details' => ['DetailsController', 'details'],
         'signup' => ['AuthController', 'signup'],
         'signin' => ['AuthController', 'login'],
+        'chat' => ['MessageController', 'chat'],
     ],
     'POST' => [
         'submit' => ['DetailsController', 'save'],
