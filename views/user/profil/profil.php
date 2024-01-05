e5e188ecb5b251da16e303eb85d0a0b8763f123d
OmarENNEDDI <enneddiomar@gmail.com>
2024-01-05 09:41:44 +0100

    pulling changes

--- views/user/profil/profil.php
+++ views/user/profil/profil.php
@@ -218,25 +218,7 @@
                                         $3467
                                     </div>
                                 </div>
-                            </li>
-                            <li class="py-3 sm:py-4">
-                                <div class="flex items-center">
-                                    <div class="flex-shrink-0">
-                                        <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image">
-                                    </div>
-                                    <div class="flex-1 min-w-0 ms-4">
-                                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                            Michael Gough
-                                        </p>
-                                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                            email@windster.com
-                                        </p>
-                                    </div>
-                                    <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                        $67
-                                    </div>
-                                </div>
-                            </li>
+                            </li> 
                             
                         </ul>
                     </div>
